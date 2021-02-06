85e3866ecf52e5d38d9e73652e4f4d1260d7ce69
udaymondal <udaysk.mondal@gmail.com>
2021-02-06 17:20:39 +0600

    finish

--- script.js
+++ script.js
@@ -29,18 +29,35 @@ function handleQuantity(isIncreased, flightClass){
         newClassCount = classCount - 1;
     }
     classInput.value = newClassCount; 
-    let subtotal = 0;
-    if(flightClass == 'firstClassInput'){
-       subtotal = newClassCount * 150;
-    }
-    if(flightClass == 'economyClassInput'){
-        subtotal = newClassCount * 100;
-    }
-    //subtotal = firstClassTotal + economyClassTotal;
-    document.getElementById('subtotal').innerText = subtotal;
+    
+    calculateTotal()
+}
+
+function calculateTotal(){
+    const firstClassInput = document.getElementById('firstClassInput');
+    const firstClassCount = parseInt(firstClassInput.value);
+
+    const economyClassInput = document.getElementById('economyClassInput');
+    const economyClassCount = parseInt(economyClassInput.value);
+
+    const subTotal = (firstClassCount * 150) + (economyClassCount * 100);
+    document.getElementById('subtotal').innerText = subTotal;
+
+    const tax = subTotal * 0.1;
+    document.getElementById('vatPrice').innerText = tax;
+
+    const inTotal = subTotal + tax;
+    document.getElementById('totalPrice').innerText = inTotal;
+
+    document.getElementById('grandTotal').innerText = inTotal;
+
 }
 
 const bookNow = document.getElementById('bookNow');
 bookNow.addEventListener('click', function(){
-    console.log(subtotal);
+    const bookingForm = document.getElementById('bookingForm');
+    bookingForm.style.display = 'none';
+    const bookingConfirm = document.getElementById('bookingConfirm');
+    bookingConfirm.style.display = 'block';
+
 })
